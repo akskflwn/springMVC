4f40b3a025d0523a6dce203c6d2e83d9f518968f
akskflwn <akskflwn@gmail.com>
2023-01-06 14:02:26 +0900

    Servlet MemberForm 만들기 예제

---
+++ servlet/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,52 @@
+package hello.servlet.domain.member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    public void save() throws Exception {
+        //given
+        Member member = new Member("hello",20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll(){
+        Member member1  = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> all = memberRepository.findAll();
+
+        assertThat(all.size()).isEqualTo(2);
+        assertThat(all).contains(member1,member2);
+
+
+
+
+    }
+}
