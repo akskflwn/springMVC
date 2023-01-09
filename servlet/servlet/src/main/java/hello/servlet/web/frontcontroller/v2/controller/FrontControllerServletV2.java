2c55a0e0d77c20ac8324d7e4a6ed046535a6a820
akskflwn <akskflwn@gmail.com>
2023-01-09 16:52:54 +0900

    MVC 패턴 V3(서블릿 종속성 제거)

--- servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/FrontControllerServletV2.java
+++ servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/FrontControllerServletV2.java
@@ -37,7 +37,7 @@ public class FrontControllerServletV2 extends HttpServlet {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
-
+        //c+Alt B 는 구현체 검색가능
         MyView view = controller.process(request,response);
         view.render(request,response);
 
