ce4bf2937529df94a4f67848a3978041af061e7b
akskflwn <akskflwn@gmail.com>
2023-01-09 17:19:02 +0900

    단순하고 실용적인 Controller V4

--- servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -2,12 +2,9 @@ package hello.servlet.web.frontcontroller;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Map;
 
 public class MyView {
